32d7b303ef8772a3769e4714e2760915a8028f10
Elvis Rodrigues <elvisear@hotmail.com>
2023-11-27 14:54:21 -0300

    novas funcionalidades

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onscroll = function () {
+  scroll();
+};
+function scroll() {
+  const btn = document.getElementById("topo");
+  if (document.documentElement.scrollTop > 200) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+}
+function backToTop() {
+  document.documentElement.scrollTop = 0;
+}
